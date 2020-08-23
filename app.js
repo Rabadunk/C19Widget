021713f78c7385aa6a3dc1e0a8e7764b9cd0bada
Jonathan Alphono <jono@Jonathans-MacBook-Pro.local>
2020-08-24 04:33:21 +1200

    added express

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 let firebase = require('firebase');
-let fs = require('fs');
-let vega = require('vega')
 
 require('firebase/database');
 require('dotenv').config();
@@ -21,27 +19,13 @@ let database = firebase.database(app);
 
 console.log(database.ref("Locations"));
 
-var stackedBarChartSpec = require('./stacked-bar-chart.spec.json');
 
-// create a new view instance for a given Vega JSON spec
-var view = new vega
-  .View(vega.parse(stackedBarChartSpec))
-  .renderer('none')
-  .initialize();
+var express = require('express');
+var server = express();
+var path = require('path');
 
-// generate static PNG file from chart
-view
-  .toCanvas()
-  .then((canvas) => {
-    // process node-canvas instance for example, generate a PNG stream to write var
-    const out = fs.createWriteStream(__dirname + '/test.jpeg');
-    const stream = canvas.createJPEGStream();
-    stream.pipe(out);
-  })
-  .catch(function (err) {
-    console.log("Error writing PNG to file:")
-    console.error(err)
-  });
-// END vega-demo.js
+server.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-
+server.listen(8080);
