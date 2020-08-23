e55d088396728a62760c19e3718add52e4c8587e
Jonathan Alphono <jono@Jonathans-MacBook-Pro.local>
2020-08-24 04:07:06 +1200

    figured out how to create graph images

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 let firebase = require('firebase');
+let fs = require('fs');
+let vega = require('vega')
+
 require('firebase/database');
 require('dotenv').config();
 
@@ -17,3 +20,28 @@ let database = firebase.database(app);
 
 
 console.log(database.ref("Locations"));
+
+var stackedBarChartSpec = require('./stacked-bar-chart.spec.json');
+
+// create a new view instance for a given Vega JSON spec
+var view = new vega
+  .View(vega.parse(stackedBarChartSpec))
+  .renderer('none')
+  .initialize();
+
+// generate static PNG file from chart
+view
+  .toCanvas()
+  .then((canvas) => {
+    // process node-canvas instance for example, generate a PNG stream to write var
+    const out = fs.createWriteStream(__dirname + '/test.jpeg');
+    const stream = canvas.createJPEGStream();
+    stream.pipe(out);
+  })
+  .catch(function (err) {
+    console.log("Error writing PNG to file:")
+    console.error(err)
+  });
+// END vega-demo.js
+
+
