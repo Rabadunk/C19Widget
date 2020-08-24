9f3aa2c4dd5bc1f1832c7af47c0d14851d04bd3a
Jonathan Alphono <jono@Jonathans-MacBook-Pro.local>
2020-08-24 21:57:26 +1200

    added animations and color

--- api/app.js
+++ api/app.js
@@ -20,7 +20,7 @@ async function buildChart() {
     let x = 10;
     let minY = 30;
     let maxHeight = 120;
-    const d3n = new D3Node({styles:'.bar {fill: #a9a9a9;opacity: 0.6;}.title{font-family:helvetica;font-size:13px;}.label{font-family:helvetica;font-size:8px;}'});
+    const d3n = new D3Node({styles:'.blah{fill:#fddb53;}.bar:hover{fill:#a9a9a9;opacity: 0.6;cursor:pointer;}.bar {fill:#fddb53;animation: load 2s;}.title{font-family:helvetica;font-size:13px;}.label{font-family:helvetica;font-size:8px;}@keyframes load { from{width:0px} to(width:80px)}'});
     let svgChart = d3n.createSVG(460, 170);
     svgChart.append('text').text('Cases by DHB').attr('class', 'title').attr('x', 176).attr('y', 20);
     await database.once('value').then(async (value) => {
@@ -32,6 +32,7 @@ async function buildChart() {
             total = placeInfo["Total"];
             let y = minY + (maxHeight - total/2);
             let height = total/2;
+            svgChart.append('text').attr('x', x).attr('y',y-5).text(`${total} cases`).attr('class', 'label blah');
             svgChart.append('g').append('rect').attr('class', 'bar').attr('x', x).attr('y', y).attr('width', '80').attr('height', height);
             svgChart.append('text').attr('x', x).attr('y',maxHeight+45).text(dhb).attr('class', 'label');
             x += 90;
