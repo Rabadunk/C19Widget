320b08dedbcc6d466bafeabbd14f76a7cf921ca8
Jonathan Alphono <jono@Jonathans-MacBook-Pro.local>
2020-08-24 21:04:39 +1200

    can make graph now

--- api/app.js
+++ api/app.js
@@ -1,12 +1,55 @@
-function buildChart(){
- 
-    const chart = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" height="100" width="100"><circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />Sorry, your browser does not support inline SVG. </svg>'
-   
-    return chart
+const D3Node = require('d3-node');
+let firebase = require('firebase');
+
+require('firebase/database');
+require('dotenv').config();
+
+let app = firebase.initializeApp({
+    apiKey: process.env.FIREBASE_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGE_SENDER_ID,
+    appId: process.env.FIREBASE_ID,
+    measurementId: process.env.MEASUREMENT_ID
+});
+
+async function buildChart() {
+    let database = firebase.database(app).ref("Locations");
+    let x = 10;
+    let minY = 30;
+    let maxHeight = 120;
+    const d3n = new D3Node({styles:'.bar {fill: #a9a9a9;opacity: 0.6;}.title{font-family:helvetica;font-size:13px;}.label{font-family:helvetica;font-size:8px;}'});
+    let svgChart = d3n.createSVG(460, 300);
+    svgChart.append('text').text('Cases by DHB').attr('class', 'title').attr('x', 176).attr('y', 20);
+    await database.once('value').then(async (value) => {
+        data = value.val();
+        let count = 0;
+        while(count < 5) {
+            placeInfo = data[count];
+            dhb = placeInfo["DHB"];
+            total = placeInfo["Total"];
+            let y = minY + (maxHeight - total/2);
+            let height = total/2;
+            svgChart.append('g').append('rect').attr('class', 'bar').attr('x', x).attr('y', y).attr('width', '80').attr('height', height);
+            svgChart.append('text').attr('x', x).attr('y',maxHeight+45).text(dhb).attr('class', 'label');
+            x += 90;
+            console.log(500 - total);
+            count++;
+        }
+    });
+
+
+
+    return d3n.svgString();
+
    
 }
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     res.setHeader('Content-Type', 'image/svg+xml');
-    res.status(200).send(buildChart());
+
+    let jazz = await buildChart();
+    res.status(200).send(jazz);
 };
--- blah.js
+++ blah.js
@@ -1,19 +1,3 @@
-let firebase = require('firebase');
-
-require('firebase/database');
-require('dotenv').config();
-
-let app = firebase.initializeApp({
-    apiKey: process.env.FIREBASE_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGE_SENDER_ID,
-    appId: process.env.FIREBASE_ID,
-    measurementId: process.env.MEASUREMENT_ID
-});
 
 
-let database = firebase.database(app);
-console.log(database.ref("Locations"));
+
